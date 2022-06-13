653d051f57758d5e31a88ebc22d46167a461d2a7
Jacob Martin <jbm6b@mtmail.mtsu.edu>
2022-06-12 22:42:35 -0500

    Added some styling and search with navbar update

--- src/scripts/events/navigationEvents.js
+++ src/scripts/events/navigationEvents.js
@@ -1,10 +1,12 @@
 import { signMeOut } from '../components/buttons/logoutBtn';
 import addEntryForm from '../components/forms/addEntryForm';
 import { getCategories } from '../../api/categoryData';
-import { noFilterEntries, showEntries } from '../components/pages/entries';
+import { emptyEntries, showEntries } from '../components/pages/entries';
 import { showCategories } from '../components/pages/categories';
 import { getEntries } from '../../api/entriesData';
-import { addButton, hideButton } from '../components/buttons/toggleCategoryButton';
+// import { addButton, hideButton } from '../components/buttons/toggleCategoryButton';
+import clearDom from '../helpers/clearDom';
+import addCategoryForm from '../components/forms/addCategoryForm';
 
 const navigationEvents = (uid) => {
   document.querySelector('#logout-btn')
@@ -12,37 +14,35 @@ const navigationEvents = (uid) => {
 
   document.querySelector('#categories').addEventListener('click', () => {
     getCategories(uid).then((categoryArray) => showCategories(categoryArray));
-    addButton();
+    // addButton();
   });
 
   document.querySelector('#create-entry').addEventListener('click', () => {
     addEntryForm({}, uid);
   });
 
+  document.querySelector('#create-category').addEventListener('click', () => {
+    addCategoryForm({}, uid);
+  });
+
   document.querySelector('#entries').addEventListener('click', () => {
     getEntries(uid).then((entryArray) => showEntries(entryArray, uid));
-    hideButton();
+    // hideButton();
+  });
 
-    document.querySelector('#search').addEventListener('input', (e) => {
-      const input = document.querySelector('#search').value.ToLowerCase();
-      if (e.keyCode === 13) {
-        getEntries(uid).then((entryArray) => {
-          const results = [];
-          entryArray.forEach((entry) => {
-            if (entry.term.ToLowerCase().includes(input)) {
-              results.push(entry);
-            }
-            if (results.length) {
-              showEntries(results, uid);
-            } else {
-              noFilterEntries();
-            }
-          });
-        });
-        document.querySelector('#search').value = '';
+  document.querySelector('#search').addEventListener('input', (e) => {
+    clearDom();
+    const input = e.target.value.trim().toLowerCase();
+    getEntries(uid).then((entryArray) => {
+      const results = entryArray.filter((entry) => entry.term.trim().toLowerCase().includes(input));
+      if (results.length) {
+        showEntries(results, uid);
+      } else {
+        emptyEntries('No entries match this search');
       }
     });
   });
+  document.querySelector('#search').value = '';
 };
 
 export default navigationEvents;
